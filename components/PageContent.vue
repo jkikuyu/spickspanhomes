6bd3cc93dcc859ba937732cd2287ef4bc311765a
Jude Kikuyu <jkikuyu@gmail.com>
2023-09-25 14:18:49 +0300

    the header, page content and footer of the pages

---
+++ components/PageContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+    <main class="max-w-4xl mx-auto">
+      <section
+        id="hero"
+        class="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 scroll-mt-40"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero earum
+        consequuntur itaque quaerat autem quibusdam, aperiam eum aliquam hic non
+        in magnam esse voluptate corrupti nesciunt adipisci expedita, mollitia
+        explicabo.
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup></script>
