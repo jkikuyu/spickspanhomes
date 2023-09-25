8755b88a50ddfcccdedc7a09ae80246a592d1168
Jude Kikuyu <jkikuyu@gmail.com>
2023-09-25 14:22:39 +0300

    megamenu to list the contractor and project types

---
+++ components/MegaMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul class="mt-3 text-[15px]">
+      <li>
+        <a href="#" class="text-sm text-[#333333] font-light py-1 block">
+          Painters
+        </a>
+      </li>
+      <li>
+        <a href="#" class="text-sm text-[#333333] font-light py-1 block">
+          Electricians
+        </a>
+      </li>
+      <li>
+        <a href="#" class="text-sm text-[#333333] font-light py-1 block">
+          Plumbers
+        </a>
+      </li>
+      <li>
+        <a href="#" class="text-sm text-[#333333] font-light py-1 block">
+          Carpenters
+        </a>
+      </li>
+      <li>
+        <a href="#" class="text-sm text-[#333333] font-light py-1 block">
+          Water Tank Cleaner
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
