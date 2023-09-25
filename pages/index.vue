e6fa8a395039dc30621434ed5e1850cde3700a51
Jude Kikuyu <jkikuyu@gmail.com>
2023-09-25 14:30:46 +0300

    mainpage in the Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <main-layout>
-    <div class="pt-[20px] pl-40 w-[calc(100%-90px)] max-w-[1200px]">
-      <div><post-main /></div>
-    </div>
-  </main-layout>
+  <top-nav />
+
+  <div>
+    <div><main-page /></div>
+  </div>
 </template>
 
 <script setup>
 const { $generalStore } = useNuxtApp();
-import MainLayout from "../layouts/MainLayout.vue";
+import BaseLayout from "../layouts/BaseLayout.vue";
 </script>
