36ec156f1a66a6aa6dbaf5643ed33d02f22bdb71
Jude Kikuyu <jkikuyu@gmail.com>
2023-09-25 14:26:10 +0300

    BaseLayout is the structure of the page

---
+++ layouts/BaseLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="baselayout">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
