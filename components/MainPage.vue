c48a6b347419fd943940fd601f772b79f90c1c94
Jude Kikuyu <jkikuyu@gmail.com>
2023-09-25 14:23:30 +0300

    the mainpage to contain the home page content

---
+++ components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseLayout>
+    <template #header>
+      <Header />
+    </template>
+
+    <template #default>
+      <PageContent />
+    </template>
+
+    <template #footer>
+      <Footer />
+    </template>
+  </BaseLayout>
+</template>
+<script setup>
+import BaseLayout from "../layouts/BaseLayout.vue";
+</script>
