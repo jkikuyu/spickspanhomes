9cf373322cbaa7ff4c1663e4fe4df14be7b240d0
Jude Kikuyu <jkikuyu@gmail.com>
2023-11-03 01:16:26 +0300

    -clean up of RadioButton component

--- components/RadioButton.vue
+++ components/RadioButton.vue
@@ -3,7 +3,8 @@
     <input
       :type="inputType"
       name="default_radio"
-      value=""
+      v-model="inputComputed"
+      :value="value"
       class="bg-gray-100 w-4 h-4 text-blue-600 accent-blue-500 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
     />
   </div>
@@ -11,23 +12,13 @@
 
 <script setup>
 const emit = defineEmits(["update:input"]);
-const props = defineProps([
-  "input",
-  "placeholder",
-  "inputType",
-  "inputName",
-  "max",
-  "autoFocus",
-  "error",
-  "selected",
-]);
-const { input, placeholder, inputType, max, autoFocus, error } = toRefs(props);
-
-onMounted(() => {
+const props = defineProps(["input", "inputType", "inputName", "value"]);
+const { input, inputType, inputName, value } = toRefs(props);
+/* onMounted(() => {
   if (autoFocus.value) {
     document.getElementById(`input-$(placeHolder.value)`).focus();
   }
-});
+}); */
 const inputComputed = computed({
   get: () => input.value,
   set: (val) => emit("update:input", val),
